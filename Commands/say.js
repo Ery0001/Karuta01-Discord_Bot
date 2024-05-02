da68740b05b06c4c28d60d82f01ab2cb596910f1
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-05-03 04:33:51 +0800

    Update say.js

--- Commands/say.js
+++ Commands/say.js
@@ -11,25 +11,22 @@ module.exports.name = "say"*/
 module.exports.run = (client, message, args) => {
   let toSay = args.join(" ");
   let textChannel = message.mentions.channels.first();
-  let attachmentsText = "";
 
   // Check if there's any text to send
   if (!toSay && !message.attachments.size) {
     return message.channel.send("You have to provide something.");
   }
 
-  // Concatenate text with attachment URLs
-  if (toSay) {
-    attachmentsText += toSay + "\n";
-  }
+  let messageContent = toSay;
 
-  // Concatenate attachment URLs
+  // Concatenate attachment URLs to the message content
   message.attachments.forEach(attachment => {
-    attachmentsText += attachment.url + "\n";
+    messageContent += "\n" + attachment.url;
   });
 
   // Send the combined text and attachments
-  message.channel.send(attachmentsText);
+  message.channel.send(messageContent);
 };
 
 module.exports.name = "say";
+
