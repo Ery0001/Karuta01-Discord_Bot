65275e1c7574708ede2ea7adb71ab45baa20ea3d
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-05-03 04:21:00 +0800

    Update say.js

--- Commands/say.js
+++ Commands/say.js
@@ -1,4 +1,4 @@
-module.exports.run = (client, message, args) => {
+/*module.exports.run = (client, message, args) => {
   let toSay = args.join(" ")
   let textchannel = message.mentions.channels.first()
   if(!toSay) return message.channel({content: "You have to provide something"})
@@ -6,4 +6,26 @@ module.exports.run = (client, message, args) => {
   
 }
 
-module.exports.name = "say"
+module.exports.name = "say"*/
+
+module.exports.run = (client, message, args) => {
+  let toSay = args.join(" ");
+  let textChannel = message.mentions.channels.first();
+
+  // Check if there's any text to send
+  if (!toSay && !message.attachments.size) {
+    return message.channel.send("You have to provide something.");
+  }
+
+  // Send the text
+  if (toSay) {
+    message.channel.send(toSay);
+  }
+
+  // Send the attachments
+  message.attachments.forEach(attachment => {
+    message.channel.send({ files: [attachment.url] });
+  });
+};
+
+module.exports.name = "say";
