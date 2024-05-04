3cf893c6afb69ef78970df366c147e4b5231ea36
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-05-04 21:28:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -91,13 +91,14 @@ client.on("messageCreate", message => {
   }*/
   if (message.content === "embed02") {
     let embed = new Discord.MessageEmbed()
-      .setTitle("Domain Rules")
-      .setDescription("- Within this realm of Discord, let all know that names and avatars must uphold honor and dignity. May all who enter these halls do so with respect and kindness, for here, we reject toxicity and embrace harmony. Let our words be as elegant as the cherry blossoms in spring. Profanity is unwelcome, and any speech that belittles another is forbidden. In this sanctuary of Haruki, let there be no room for bullying or harassment. Let peace reign, and may all find refuge here. Let our intentions be pure, like freshly fallen snow. Lewdness has no place here; our interactions shall be marked by grace and dignity. The privacy of others shall be respected as sacred. Revealing personal information is strictly prohibited; here, privacy is honored above all.\n\n[春樹/Haruki]")
+      .setTitle("EDICT OF UNITY")
+      .setDescription("\n- Within this realm of Discord, let all know that names and avatars must uphold honor and dignity.\n- May all who enter these halls do so with respect and kindness, for here, we reject toxicity and embrace harmony. Let us show no tolerance for prejudice or hate.\n- Let our words be as elegant as the cherry blossoms in spring. Profanity is unwelcome, and any speech that belittles another is forbidden.\n- In this sanctuary of Haruki, let there be no room for bullying or harassment. Let peace reign, and may all find refuge here.\n- Let our intentions be pure, like freshly fallen snow. Lewdness has no place here; our interactions shall be marked by grace and dignity.\n- The privacy of others shall be respected as sacred. Revealing personal information is strictly prohibited; here, privacy is honored above all.\n")
       .setColor("#FF5B5B")
-      .setImage("https://ik.imagekit.io/Zedi/20240504_210654.jpg?updatedAt=1714828320495");
-
-    message.channel.send({ embeds: [embed] });
-  }
+      .setImage("https://ik.imagekit.io/Zedi/20240504_210654.jpg?updatedAt=1714828320495")
+      .setFooter("Haruki Empire - 春樹")
+    
+    message.channel.send({ embeds: [embed] })
+  } 
   if (message.content === "embed3") {
     let embed = new Discord.MessageEmbed()
       .setDescription("Haruki Village is a Japanese theme Village And Found on the Minecraft Server named Fallenskymc.\n \n The Main Purpose of The server is to guide New members Of Haruki village and easily access the Village Rules.\n \n FALLENSKYMC SERVER INFORMATION: \n DISCORD/ https://bit.ly/fsmcdiscord \n Server IP: play.fallenskymc.com \n Port: 19132 \n \n HARUKI FOUNDER: \n EmpireOfZedi16 \n EmperorPads \n \n DISCORD SERVER CREATOR: \n EmpireOfZedi16")
