187e2245c70fa3c723bdf5fe4f09dbc384272e90
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-05-26 02:52:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -363,11 +363,11 @@ if (hasMention && (messageContent.includes("schedule") || messageContent.include
 
     upcomingSchedules.slice(0, 5).forEach(schedule => {
         const timeFormatted = moment(schedule.nextRun).format('MMM Do, HH:mm');
-        const messageField = schedule.nextRun < currentTime ? `${schedule.message}` : `${schedule.message} :white_check_mark:`;
+        const messageField = schedule.nextRun < currentTime ? `${schedule.message}` : `${schedule.message}`;
         const statusField = schedule.nextRun < currentTime ? '' : ':white_check_mark:';
         embed.addField('Time', timeFormatted, true);
         embed.addField('Message', messageField, true);
-        embed.addField('Status', statusField, true); // Add Status field
+        embed.addField('Status', statusField, true);
     });
 
     message.reply({ embeds: [embed] });
