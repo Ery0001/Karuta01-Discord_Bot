876cee54f4ed0ff496a6ba7f89724214774acfe5
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-05-26 15:13:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ if (hasMention && hasMentionSchedule) {
     const currentTime = new Date();
     const phTimezone = 'Asia/Manila';
     const todayStart = moment.tz(currentTime, phTimezone).startOf('day');
-    const todayEnd = moment(today).add(1, 'day');
+    const todayEnd = moment.tz(today).add(1, 'day');
    
     let todaysSchedules = schedules
         .map(schedule => {
