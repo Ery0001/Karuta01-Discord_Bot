0a5d887288c27383756da83b69f87c68d49813e7
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2024-06-13 03:14:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -478,23 +478,21 @@ client.on('ready', async () => {
     })
 
     // Schedule multiple messages
-    scheduleTempMessage('0 20 * * *', 'Asia/Manila', 'Guild activities are approaching in 30 minutes.', "1237979376872718439",500);
-    scheduleTempMessage('20 20 * * *', 'Asia/Manila', 'Guild activities are approaching in 10 minutes.', "1237979376872718439",500);
+    scheduleTempMessage('0 20 * * *', 'Asia/Manila', '@everyone Guild activities are approaching in 30 minutes.', "1237979376872718439",1000);
+    scheduleTempMessage('20 20 * * *', 'Asia/Manila', '@everyone Guild activities are approaching in 10 minutes.', "1237979376872718439",2000);
     
-    scheduleTempMessage('25 20 * * *', 'Asia/Manila', '@everyone Time for the Guild bath in 5 minutes. Join us or miss out, your loss!', "1237979376872718439",3600000);
-    scheduleTempMessage('30 20 * * *', 'Asia/Manila', 'Guild bath is starting!<:Stare_erythrina:1238029119632048159>', "1237979376872718439",2000);
-    scheduleTempMessage('35 20 * * *', 'Asia/Manila', '@everyone Get ready for the Guild boss battle in 5 minutes! Don\'t slack off now, we need everyone!', "1237979376872718439",3600000);
-    scheduleTempMessage('40 20 * * *', 'Asia/Manila', 'Guild boss is starting!<:Stare_erythrina:1238029119632048159>', "1237979376872718439",2000);
-    scheduleTempMessage('55 20 * * 2,4,6', 'Asia/Manila', '@everyone The Guild war is about to begin in 5 minutes! Prepare yourself!', "1237979376872718439",3600000);
-    scheduleTempMessage('0 21 * * 2,4,6', 'Asia/Manila', 'Guild war is starting!<:Erythrina_happy:1249365602397716540>', "1237979376872718439",2000);
+    scheduleTempMessage('25 20 * * *', 'Asia/Manila', '@everyone Time for the Guild bath in 5 minutes. Join us or miss out, your loss!', "1237979376872718439",600000);
+    scheduleTempMessage('30 20 * * *', 'Asia/Manila', 'Guild bath is starting!<:Stare_erythrina:1238029119632048159>', "1237979376872718439",300000);
+    scheduleTempMessage('35 20 * * *', 'Asia/Manila', '@everyone Get ready for the Guild boss battle in 5 minutes! Don\'t slack off now, we need everyone!', "1237979376872718439",600000);
+    scheduleTempMessage('40 20 * * *', 'Asia/Manila', 'Guild boss is starting!<:Stare_erythrina:1238029119632048159>', "1237979376872718439",300000);
+    scheduleTempMessage('55 20 * * 2,4,6', 'Asia/Manila', '@everyone The Guild war is about to begin in 5 minutes! Prepare yourself!', "1237979376872718439",1200000);
+    scheduleTempMessage('0 21 * * 2,4,6', 'Asia/Manila', 'Guild war is starting!<:Erythrina_happy:1249365602397716540>', "1237979376872718439",900000);
     scheduleRndmMessage('30 6 * * *', 'Asia/Manila', "1237979376872718439");
 
     //every 10 minutes
     //scheduleRndmMessage('*/10 * * * *', 'Asia/Manila', '1237979377363320916');
-    scheduleTempMessage('*/1 * * * *', 'Asia/Manila', 'test1', "1237979377363320916",1000);
-    scheduleTempMessage('*/1 * * * *', 'Asia/Manila', 'test2', "1237979377363320916",2000);
-    scheduleTempMessage('*/1 * * * *', 'Asia/Manila', 'test3', "1237979377363320916",4000);
-    scheduleTempMessage('*/1 * * * *', 'Asia/Manila', 'test4', "1237979377363320916",1000);
+    //scheduleTempMessage('*/1 * * * *', 'Asia/Manila', 'test1', "1237979377363320916",1000);
+
     
      //Reminders of reminder channel
     scheduleRemindersChannel('0 7 * * *', 'Asia/Manila', "1237979376872718439",3600000);
