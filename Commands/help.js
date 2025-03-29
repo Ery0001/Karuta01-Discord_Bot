acf6c334dc98c3b9be5ccae8781604a42ea0f687
Ery0001 <zedycoolgamer1516@gmail.com>
2025-03-29 13:23:12 +0800

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -4,28 +4,22 @@ module.exports.run = (client, message, args) => {
     const announcementEmbed = new Discord.MessageEmbed()
         .setTitle("ANNOUNCEMENT COMMAND")
         .setColor("#c0c0c0")
-        .setDescription(
-            "```
-h.an \"message\" \"image_url (optional)\" \"role_id (optional)\"
+        .setDescription(```
+h.an "message" "image_url (optional)" "role_id (optional)"
 ```
-" +
-            "- Sends an announcement to a fixed channel.\n" +
-            "- Example: `h.an \"Hello everyone!\" \"https://example.com/image.jpg\" \"1354641345762955341\"`\n" +
-            "- If no image is attached, you can use an image URL instead."
-        );
+- Sends an announcement to a fixed channel.
+- Example: `h.an "Hello everyone!" "https://example.com/image.jpg" "1354641345762955341"`
+- If no image is attached, you can use an image URL instead.`);
 
     const embedEmbed = new Discord.MessageEmbed()
         .setTitle("EMBED COMMAND")
         .setColor("#c0c0c0")
-        .setDescription(
-            "```
-h.embed channel_id \"message\" \"image_url (optional)\" \"role_id (optional)\"
+        .setDescription(```
+h.embed channel_id "message" "image_url (optional)" "role_id (optional)"
 ```
-" +
-            "- Sends an embed to a specific channel.\n" +
-            "- Example: `h.embed 123456789012345678 \"Important Update!\" \"https://example.com/image.jpg\" \"1354641345762955341\"`\n" +
-            "- If no image is attached, you can use an image URL instead."
-        );
+- Sends an embed to a specific channel.
+- Example: `h.embed 123456789012345678 "Important Update!" "https://example.com/image.jpg" "1354641345762955341"`
+- If no image is attached, you can use an image URL instead.`);
 
     message.channel.send({ embeds: [announcementEmbed, embedEmbed] });
 };
