d69e17f5f19a49667be200ea2fc839ec5cb69b1d
Ery0001 <zedycoolgamer1516@gmail.com>
2025-03-29 16:50:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,7 @@ app.get("/", (req, res) => {
 const Discord = require("discord.js");
 const { Client, Intents, MessageEmbed, Collection } = require('discord.js');
 const client = new Discord.Client({
-    intents: [
-        "GUILDS", 
-        "GUILD_MESSAGES", 
-        "GUILD_MESSAGE_REACTIONS",
-        "GUILD_MEMBERS",
-        "GUILD_EMOJIS_AND_STICKERS"
-    ],
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_MESSAGE_REACTIONS", "MESSAGE_CONTENT"],
     // intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
     allowedMentions: { parse: ['users', 'roles', 'everyone'] }
 });
@@ -132,68 +126,6 @@ const scheduleEmbed = (cronTime, timezone, message, channelId, status) => {
         timezone: timezone
     });
 };
-
-
-const ErythinaMorningPhrase = [
-    "Ugh, @everyone better snap outta dreamland and hustle up on those dailies! Like, pronto!",
-    "Hey, sleepyheads! Time to rise and shine and get cracking on those dailies, cuties! @everyone",
-    "@everyone Get up! It's not like I care, but we've all got our dailies to tackle.",
-    "Ahem. Listen up, @everyone. The clock's ticking, and if you dawdle too long, you'll find yourself knee-deep in regrets. So, consider this your wake-up call.",
-    "Get your butts outta bed, @everyone! Dailies won't do themselves, you know.",
-    "Wakey-wakey, sleepyheads! Let's tackle those dailies, angels! @everyone",
-    "Hey, rise and shine, @everyone! Dailies await, whether you like it or not.",
-    "Morning, @everyone! Let's not waste time. Dailies won't wait for anyone.",
-    "Dailies time, sweethearts! @everyone.",
-    "Yo, @everyone! Time to kick yesterday's dust off and do those dailies!",
-    "Rise and grind, @everyone! Dailies aren't gonna do themselves, you know.",
-    "Dailies, darlings! @everyone.",
-    "Attention, @everyone! It's time to tackle those dailies head-on.",
-    "Hey, sleepyheads! Let's rise and conquer those dailies, cuties! @everyone.",
-    "No time for slacking, @everyone! Dailies are calling, loud and clear.",
-    "Morning, @everyone! Let's make today count. Dailies first, complaints later.",
-    "Dailies, sweethearts! @everyone.",
-    "Time's ticking, @everyone! Let's hustle up and knock out those dailies.",
-    "Wake up, @everyone! Dailies won't wait for late risers.",
-    "Alright, @everyone! Rise and grind. Dailies won't tackle themselves.",
-    "Dailies, beauties! @everyone.",
-    "Hey there, @everyone! Time to roll up those sleeves and tackle those dailies, angels!",
-    "Morning, @everyone! Let's make today's dailies count, shall we?",
-    "Enough with the snoozing, @everyone! Dailies time is now.",
-    "Dailies, darlings! @everyone.",
-    "Attention, @everyone! Let's tackle those dailies with gusto.",
-    "Wake up and smell the dailies, @everyone! No time to waste.",
-    "No excuses, @everyone! Time to dive into those dailies headfirst.",
-    "Alright, @everyone! Let's shake off yesterday's dust and tackle those dailies.",
-    "Morning, @everyone! Dailies await, so let's get to it.",
-    "Time to rise and shine, @everyone! Dailies aren't gonna do themselves.",
-    "Dailies, sweethearts! @everyone.",
-    "Get up, get moving, @everyone! Dailies won't tackle themselves.",
-    "Wakey-wakey, @everyone! Dailies are calling, loud and clear.",
-    "Enough dilly-dallying, @everyone! Dailies time is now.",
-    "Morning, @everyone! Let's kickstart the day with some dailies.",
-    "Dailies, beauties! @everyone.",
-    "Rise and grind, @everyone! Dailies await, so let's get to it.",
-    "Attention, @everyone! It's time to show those dailies who's boss.",
-    "Morning, @everyone! Let's tackle those dailies like pros.",
-    "Time's a-wasting, @everyone! Let's dive into those dailies pronto.",
-    "Up and at 'em, @everyone! Dailies await, so let's get moving.",
-    "Dailies, darlings! @everyone.",
-    "Enough lollygagging, @everyone! Dailies aren't gonna do themselves."
-];
-
-const ErythinaRemindersChannelPhrase = [
-    "Reminders channel is over here: <#1347152454210420746>. Not that I care if you forget.",
-    "The reminders channel is right here: <#1347152454210420746>. Try not to miss it, okay?",
-    "In case you can’t figure it out, the reminders channel is here: <#1347152454210420746>.",
-    "The reminders channel is over here: <#1347152454210420746>. Use it or don’t, up to you."
-];
-
-/*const ErythinaRemindersChannelPhrase = [
-    "@everyone Reminders channel is over here: <#1347152454210420746>. Not that I care if you forget.",
-    "@everyone The reminders channel is right here: <#1347152454210420746>. Try not to miss it, okay?",
-    "@everyone In case you can’t figure it out, the reminders channel is here: <#1347152454210420746>.",
-    "@everyone The reminders channel is over here: <#1347152454210420746>. Use it or don’t, up to you."
-];*/
 
 client.on("messageCreate", message => {
     if (message.content.startsWith(prefix)) {
@@ -315,194 +247,24 @@ For the full main server rules, check <#1305705930926850119>.
         }
 
         const hasMention = words.some(word =>
-  word.startsWith("@Cheongmun") ||
-  word.startsWith("Cheongmun") ||
-  word.startsWith("Cheongmn") ||
-  word.startsWith("Cheongmu") ||
-  word.startsWith("Cheongun") ||
-  word.startsWith("Cheonmun") ||
-  word.startsWith("Cheong")
-);
-
-        const hasMentionSchedule = words.some(word =>
-        word.startsWith("schedules") ||
-        word.startsWith("schedule") ||
-        word.startsWith("schedul") ||
-        word.startsWith("sched") ||
-        word.startsWith("scheds") ||
-        word.startsWith("sche")
+        word.startsWith("@Cheongmun") ||
+        word.startsWith("Cheongmun") ||
+        word.startsWith("Cheongmn") ||
+        word.startsWith("Cheongmu") ||
+        word.startsWith("Cheongun") ||
+        word.startsWith("Cheonmun") ||
+        word.startsWith("Cheong")
         );
 
-        const hasGreet = words.some(word =>
-        word.startsWith("hello") ||
-        word.startsWith("helloo") ||
-        word.startsWith("helllo") ||
-        word === "halo" ||
-        word === "helo" ||
-        word === "elo" ||
-        word === "alo" ||
-        word === "hi" ||
-        word === "hey" ||
-        word.startsWith("greetings") ||
-        word.startsWith("howdy") ||
-        word.startsWith("hey there") ||
-        word.startsWith("what's up") ||
-        word.startsWith("aloha") ||
-        word.startsWith("bonjour") ||
-        word.startsWith("ciao") ||
-        word.startsWith("guten tag") ||
-        word.startsWith("namaste") ||
-        word.startsWith("salaam") ||
-        word.startsWith("konnichiwa") ||
-        word.startsWith("annyeong") ||
-        word.startsWith("zdravstvuyte") ||
-        word.startsWith() === "hiya" ||
-        word === "hola" ||
-        word === "sup" ||
-        word === "yo" ||
-        word.startsWith() === "yoo"
-        );
-        const hasCompliment1 = words.some(word =>
-        word.startsWith("awesome") || 
-        word.startsWith("amazing") || 
-        word.startsWith("fantastic") || 
-        word.startsWith("great") || 
-        word.startsWith("wonderful") || 
-        word.startsWith("excellent") || 
-        word.startsWith("brilliant") || 
-        word.startsWith("outstanding") || 
-        word.startsWith("remarkable") || 
-        word.startsWith("impressive") || 
-        word.startsWith("fabulous") || 
-        word.startsWith("incredible") || 
-        word.startsWith("spectacular") || 
-        word.startsWith("marvelous") || 
-        word.startsWith("terrific") || 
-        word.startsWith("superb") || 
-        word.startsWith("good")
-        );
-        const hasSubject = words.some(word =>
-            word.startsWith("you're") ||
-            word.startsWith("you are") ||
-            word.startsWith("your") ||
-            word.toLowerCase() === "you" ||
-            word.startsWith("you is") ||
-            word.startsWith("u") ||
-            word.startsWith("she") ||
-            word.toLowerCase() === "her" ||
-            word.startsWith("them") ||
-            word.startsWith("female") ||
-            word.startsWith("bot") ||
-            word.startsWith("ai") ||
-            word.startsWith("a.i") ||
-            word.toLowerCase() === "the" ||
-            word.toLowerCase() === "that" ||
-            word.startsWith("this") ||
-            word === "is"
-        );
-
-        const hasSaidQuiet = words.some(word =>
-            word.startsWith("quiet") ||
-            word === "shat" ||
-            word === "shut" ||
-            word === "shap" ||
-            word === "stfu" ||
-            word === "stf" ||
-            word === "sh" ||
-            word === "shh" ||
-            word === "shh" ||
-            word === "shhh" ||
-            word === "shhhh" ||
-            word === "shhhhh" ||
-            word === "shhhhhh" ||
-            word === "shhhhhhh" ||
-            word === "shhhhhhhh" ||
-            word === "sshhhhhhh" ||
-            word === "sshhhhhh" ||
-            word.startsWith("annoying") ||
-            word.startsWith("loud")
-        );
-        const hasThanks = words.some(word =>
-    word.startsWith("thanks") ||
-    word.startsWith("thank") ||
-    word.startsWith("thankful") ||
-    word.startsWith("grateful") ||
-    word.startsWith("appreciate") ||
-    word.startsWith("appreciated") ||
-    word.startsWith("appreciation") ||
-    word.startsWith("gratitude") ||
-    word.startsWith("cheers") ||
-     word.toLowerCase() === "ta" ||
-    word.startsWith("much obliged") ||
-    word.startsWith("big thanks") ||
-    word.startsWith("heartfelt thanks") ||
-    word.startsWith("sincere thanks") ||
-    word.startsWith("many thanks") ||
-    word.startsWith("i appreciate it") ||
-    word.startsWith("i'm thankful") ||
-    word.startsWith("thank you") ||
-    word.startsWith("terima kasih") || // Indonesian/Malay
-    word.startsWith("salamat") || // Filipino/Tagalog
-    word.startsWith("khob khun") || // Thai
-    word.startsWith("khop kun") || // Thai
-    word.startsWith("cảm ơn") || // Vietnamese
-    word.startsWith("ကျေးဇူးတင်ပါတယ်") || // Burmese
-    word.startsWith("ຂອບ​ໃຈ") || // Lao
-    word.startsWith("អរគុណ") // Khmer
-);
         const hasAsk = words.includes("who");
         const hasInvite = words.includes("invite");
         const hasLink = words.includes("link");
         const wordquite = words.includes("quite");
-        if (hasGreet && hasMention) {
-            const ErythinaRelpyGreetings = [
-                `Oh, you’re here. nice to meet you.`,
-                `Oh, it’s you. hello..`,
-                `Pretend that I am not here`,
-                `Hi`
-            ];
-            const randomReplyGreeting = ErythinaRelpyGreetings[Math.floor(Math.random() * ErythinaRelpyGreetings.length)];
-            message.reply(randomReplyGreeting);
-        }
-        if (hasMention && hasSubject && hasCompliment1) {
-            const ErythinaRelpyCompliment = [
-                `Oh, thank you`,
-                `Oh, it was nothing.`,
-                `I know.`
-            ];
-            const randomReplyCompliment = ErythinaRelpyCompliment[Math.floor(Math.random() * ErythinaRelpyCompliment.length)];
-            message.reply(randomReplyCompliment);
-        }
-        if (hasMention && hasCompliment1) {
-            if (messageContent.length < 13) {
-            message.reply(`...`);
-         }
-        }
-        // if (hasInvite && hasLink) {
-        //     message.reply(`take it then <:Stare_erythrina:1238029119632048159>nhttps://bit.ly/Noblese`);
-        // }
-        if (hasMention && hasThanks) {
-            // message.reply(`you're welcome <@!${message.author.id}><:Stare_erythrina:1238029119632048159>`);
-            message.reply(`you're welcome <@!${message.author.id}>`);
-        }
+        
         if (hasMention && hasAsk) {
             message.reply(`Greetings, I am Cheongmun, developed by <@894665274123513856>, And one of the previous sect leader of mount hua sect.\nRead here to know more about me: https://return-of-the-blossoming-blade.fandom.com/wiki/Cheongmun`);
         }
-
-        if (hasMention && hasSaidQuiet) {
-            const ErythinaRelpy = [
-                `eh?`,
-                `meanie :<`
-            ];
-            const randomReply = ErythinaRelpy[Math.floor(Math.random() * ErythinaRelpy.length)];
-            message.reply(randomReply);
-        }
-        if (hasMention && wordquite) {
-          if (messageContent.length < 21) {
-            message.reply(`You spelled "quiet" wrong>`);
-         }
-        }
-
+        
     }
 });
 
@@ -656,21 +418,5 @@ client.on("messageCreate", async (message) => {
         }
     });
 });
-
-
-// client.on('guildMemberAdd', member => {
-//     if (member.user.bot) return; // Avoid greeting bots
-    
-//     const channelId = '1292688019811336202'; // Replace with the actual channel ID
-//     const channel = client.channels.cache.get(channelId);
-//     const welcomeMessage = `Hello, welcome <@${member.id}>! this is the main karuta channel, enjoy your stay!`;
-
-//     if (channel) {
-//         channel.send(welcomeMessage).catch(console.error);
-//     } else {
-//         console.log('Welcome channel not found.');
-//     }
-// });
-
 
 client.login(process.env.token);
