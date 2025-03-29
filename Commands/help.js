2484f2a7e93ff8821414f70ee0e7618ec1cf8720
Ery0001 <zedycoolgamer1516@gmail.com>
2025-03-29 13:17:31 +0800

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -5,28 +5,28 @@ module.exports.run = (client, message, args) => {
         .setTitle("ANNOUNCEMENT COMMAND")
         .setColor("#c0c0c0")
         .setDescription(
-            "```
+            '```
 h.an \"message\" \"image_url(optional)\" \"role_id(optional)\"
 ```
 " +
             "- Sends an announcement to a fixed channel.\n" +
             "- Example: `.an \"Hello everyone!\" \"https://example.com/image.jpg\" \"1354641345762955341\"`\n" +
-            "- If no image is attached, you can use an image URL instead."
-        )
+            "- If no image is attached, you can use an image URL instead.'
+        );
         .setFooter("Need more help? Contact an admin!");
 
     const embedEmbed = new Discord.MessageEmbed()
         .setTitle("EMBED COMMAND")
         .setColor("#c0c0c0")
         .setDescription(
-            "```
+            '```
 h.embed channel_id \"message\" \"image_url(optional)\" \"role_id(optional)\"
 ```
 " +
             "- Sends an embed to a specific channel.\n" +
             "- Example: `.embed 123456789012345678 \"Important Update!\" \"https://example.com/image.jpg\" \"1354641345762955341\"`\n" +
-            "- If no image is attached, you can use an image URL instead."
-        )
+            "- If no image is attached, you can use an image URL instead.'
+        );
         .setFooter("Use quotes for multi-word messages and optional parameters.");
 
     message.channel.send({ embeds: [announcementEmbed, embedEmbed] });
