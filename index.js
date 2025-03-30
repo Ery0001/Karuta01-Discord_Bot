e08e011a86f6933a9e6282163b9486d8f313bec6
Ery0001 <zedycoolgamer1516@gmail.com>
2025-03-30 19:13:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -191,29 +191,6 @@ const userMessageCounts = new Collection();
 const MESSAGE_LIMIT = 20;
 const MESSAGE_LENGTH_THRESHOLD = 25;
 
-// client.on('messageCreate', message => {
-//     if (message.author.bot) return;
-    
-//     const triggerWords = ['kd', 'k!d', 'k!drop'];
-//     if (triggerWords.includes(message.content.toLowerCase()) && message.channel.id !== DROP_CARDS_CHANNEL_ID) {
-//         message.reply(`The place for drawing cards is <#${DROP_CARDS_CHANNEL_ID}>. Head there to continue. ${EMOTE_ID}`);
-//     }
-
-//         // Message spam detection (only count messages with over 25 characters)
-//     if (!MAIN_CHAT_CHANNELS.includes(message.channel.id) && message.content.length > MESSAGE_LENGTH_THRESHOLD) {
-//         const userId = message.author.id;
-//         const userMessages = userMessageCounts.get(userId) || 0;
-
-//         if (userMessages >= MESSAGE_LIMIT) {
-//             message.reply(`You're quite active! If you’d like to continue chatting, the main discussion happens here: <#${MAIN_CHAT_CHANNELS[0]}>.`);
-//             userMessageCounts.set(userId, 0); // Reset count after notification
-//         } else {
-//             userMessageCounts.set(userId, userMessages + 1);
-//         }
-//     }
-    
-// });
-
 const KARUTA_ID = "646937666251915264";
 const TRACKED_ROLES = ["1354641345905561884", "1354641345905561883", "1354641345762955338"];
 const NOTIFY_CHANNEL_ID = "1355431839640322158";
