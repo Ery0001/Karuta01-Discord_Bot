ec87f020cdcb1e03512e31f4ec4c176d25611687
Ery0001 <zedycoolgamer1516@gmail.com>
2025-04-04 09:57:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -318,6 +318,30 @@ client.on("messageCreate", async (message) => {
   });
 });
 
+client.on('guildMemberAdd', async (member) => {
+    console.log(`${member.user.tag} joined!`); // Debugging
+
+    if (member.user.bot) return; 
+
+    const REACT_EMOJI = "<:Mount_Hua_Sect_Symbol:1354789652606750950>";
+    const channelId = '1354694726296797274';
+    const channel = client.channels.cache.get(channelId);
+    
+    if (!channel) {
+        console.log('Channel not found.');
+        return;
+    }
+
+    try {
+        const welcomeMessage = `Welcome to Lian Faction <@${member.id}>. Please wait here.`;
+        const sentMessage = await channel.send(welcomeMessage);
+        await sentMessage.react(REACT_EMOJI);
+        console.log('Welcome message sent and reacted!');
+    } catch (error) {
+        console.error('Error sending message or adding reaction:', error);
+    }
+});
+
 async function processContributionEmbed(embed, message) {
   if (!embed.fields.length) return;
   const contributionField = embed.fields[0]?.value;
